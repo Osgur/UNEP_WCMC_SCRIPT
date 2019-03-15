bfd206747449ce40d5ffe06541d13c020eb95cd4
Osgur McDermott Long <36760807+Osgur@users.noreply.github.com>
2019-03-15 13:03:45 +0000

    Create WDPA_Verified_Check.R

---
+++ WDPA_Verified_Check.R
@@ -0,0 +1,89 @@
+#install.packages(c("sp", "raster", "rgdal", "rgeos", "gtools", "spdep", "foreign", "stringr", "devtools", "sf", "xlsx", "plyr", "readxl", "knitr", "dplyr", "shiny", "tidyverse"))
+library(sp)  # vector data
+library(raster)  # raster data
+library(rgdal)  # input/output, projections
+library(rgeos)  # geometry ops
+library(gtools)
+library(spdep)  # spatial dependence
+library(foreign)
+library(stringr)
+library(devtools)
+library(sf)
+library(xlsx)
+library(plyr)
+library(readxl)
+library(knitr)
+library(dplyr)
+library(shiny)
+library(tidyverse)
+library(png)
+
+Path <- "C:/Users/OsgurM/Downloads/WDPA_MASTER.gdb"
+ogrListLayers(Path)
+
+#######Read in the data layers######
+
+#Public
+WDPA_Py <- data.frame(read_sf(Path, layer = "WDPA_poly_Mar2019"))
+WDPA_Pt <- data.frame(read_sf(Path, layer = "WDPA_point_Mar2019"))
+
+#Restricted for China
+China_NR_Res<- data.frame(read_sf(Path, layer = "CHN_restricted_Nov2018_NR"))
+China_NNR_Res <- data.frame(read_sf(Path, layer = "CHN_restricted_Nov2018_NNR"))
+
+#Restricted for Rusia
+Russia_Py_Res <- data.frame(read_sf(Path, layer = "RUS_only_for_public"))
+Russia_Pt_Res <- data.frame(read_sf(Path, layer = "RUSpt_only_public_New"))
+
+#Restricted for Saint Helena
+SHN_Res <- data.frame(read_sf(Path, layer = "SHN_restricted_July2018"))
+
+#Restricted for Estonia
+EST_Res <- data.frame(read_sf(Path, layer = "EST_restricted_Aug2014_New"))
+
+
+#Combine all layers
+Combined <- bind_rows(WDPA_Py, WDPA_Pt,China_NR_Res, China_NNR_Res, Russia_Py_Res, Russia_Pt_Res, SHN_Res, EST_Res)
+
+
+#identify numbers for verification by ISO3
+Verified <- Combined %>%
+  select(WDPAID, ISO3, DESIG_TYPE, VERIF) %>%
+  distinct()%>%
+  group_by(ISO3) %>%
+  count(VERIF) %>%
+  spread(VERIF, n)   
+
+
+#subset dataset by International and Regional data types
+Int_Reg <- subset(Combined, DESIG_TYPE %in% c("International", "Regional"))
+
+Reg_Int_Sum <- Int_Reg %>%
+  select(WDPAID, ISO3, DESIG_TYPE, VERIF) %>%
+  distinct()%>%
+  group_by(ISO3) %>%
+  summarise(Int_Reg_Count = n())
+
+#Set NA to 0
+Verified[is.na(Verified)] <- 0 
+
+Verified <- merge(Verified, Reg_Int_Sum, by = "ISO3",  all = T)  
+Verified$Total <- rowSums(Verified[,2:4])
+
+
+#Set NA to 0
+Verified[is.na(Verified)] <- 0 
+
+#Set the Colnames 
+colnames(Verified)[5] <- "Number International or Regional"
+
+#splits transboundary sites and assigns them to all countries
+Verified <- Verified %>%
+  mutate(ISO3 = strsplit(as.character(ISO3), ";")) %>%
+  unnest() %>%
+  group_by(ISO3) %>%
+  summarize_if(is.numeric, sum, na.rm=TRUE)
+
+
+
+write.csv(Verified, "WDPA_Verified.csv", row.names = F)
