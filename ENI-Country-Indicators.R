65c843a3260e539ed948c62fc83cba9692adfbcb
Osgur McDermott Long <36760807+Osgur@users.noreply.github.com>
2019-06-26 11:15:37 +0100

    Update ENI-Country-Indicators.R

--- ENI-Country-Indicators.R
+++ ENI-Country-Indicators.R
@@ -1,133 +1,95 @@
-library(rgdal)
-library(dplyr)
-library(sf)
 library(arcgisbinding)
+library(dplyr)
+library(rgdal)
 library(plyr)
+library(tidyr)
+library(reshape2)
 library(raster)
-library(rgeos)
-library(ggplot2)
-library(lwgeom)
+library(rasterVis)
+library(RColorBrewer)
+library(tidyverse)
+library(data.table)
+###Layer Preparation
+#WDPA - Remove Mab sites and proposed buffer points and join to polygon. Intersect with EEZ_WVS LAyer. Dissolve by DesigType or DesigEng depending on requirement (filter out data required first).  Calculate area using mollweide world. 
 
+###KBA intersect with EEZ_WVS layer and filter out by only confirmed for KBA IBA and AZE. Dissolve and calculate area by mollweide world. 
 
+#AZE filter KBA
+
+#Critical Habitat - Join Crit Hab to rasterised version of EEZ_WVS. Set pixel size so equals 1Km2 
+
+#Set configuration
 arc.check_product()
+options(scipen=999)
+#setwd to your folder containing the LAyers shapfiles or file geodatabase
+setwd("filepath")
+ogrListLayers(getwd())
 
-Land.arc <- arc.open("C:/Users/OsgurM/Downloads/gadm36_shp/gadm36.shp")
-EEZ.arc <- arc.open("C:/Users/OsgurM/Downloads/World_EEZ_v10_20180221/eez_v10.shp")
-KBA.arc <-  arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/Ocean+/Ocean+.gdb/KbaMapGlobal_POL_CopyFeatures")
-CritHab <- stack("C:/Users/OsgurM/Downloads/WCMC_043_GlobalCH_IFCPS6_2017/DataPack_WCMC_043_GlobalCH_IFCPS6_2017/01_Data/crhab_mt.ovr")
-WDPA.arc <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/Ocean+/Ocean+.gdb/WDPA_poly_Jun2019_CopyFeatures")
+#REad in the EEZ_WVS layer that has already had area calculated using mollweide world
+EEZ <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/ENI-Analysis.gdb/Country_Boundaries_EEZ_ABNJ") %>%
+  arc.select(fields = "*") 
 
+##Read in WDPA dissolved using DESIG_ENG to identify ramsar and WHS sites
+DESIG.arc <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/ENI-Analysis.gdb/WDPA_DESIG") %>%
+  arc.select(fields = "*")%>% # read in the dataset
+  dplyr::select(-OBJECTID)%>% # remove column
+  dplyr::rename(WDPA_AREA = AREA) %>% # rename column
+  group_by(ISO3, type)%>% # group by grouping columns 
+  left_join(EEZ[,2:4],by= c("type", "ISO3"))%>% # join to EEZ layer to get total area
+  unite("DESIG_AREA", c(DESIG_ENG, type)) # join columns for easier casting
+##
+DESIG_Out <- dcast(setDT(DESIG.arc), ISO3 ~DESIG_AREA, value.var = c("WDPA_AREA", "AREA_KM2")) # cast data into readable dataframe
 
-ISO3 <- Land.arc %>%
-  arc.select(fields = "GID_0") %>%
-  distinct(GID_0) %>% 
-  filter( !grepl("/",GID_0))
-
-ISO3 <-"IRL"
-for(i in ISO3){
-
-Landsql <- paste0("GID_0 LIKE '%",i,"%'")
-EEZsql <- paste0("ISO_Ter1 LIKE '%",i,"%'")
-EEZComsql <- paste0("ISO_Ter1 LIKE '%",i,"%'")
-
-Landsub <- Land.arc %>%
-  arc.select(fields = "GID_0", where_clause = Landsql) %>%
-  arc.data2sf() %>%
-  st_cast("POLYGON")%>%
-  st_transform("+proj=moll") %>%
-  st_buffer(0) %>%
-  st_union()
-
-EEZsub <- EEZ.arc %>%
-  arc.select(fields = "*", where_clause = EEZsql) %>%
-  arc.data2sf() %>%
-  st_cast("POLYGON")%>%
-  st_transform("+proj=moll")
+##Read in WDPA dissolved for DESIG Type
+DESIG_TYPE.arc <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/ENI-Analysis.gdb/WDPA_DESIGTYPE") %>%
+  arc.select(fields = "*")%>%
+  dplyr::select(-OBJECTID)%>%
+  dplyr::rename(WDPA_AREA = AREA) %>%
+  group_by(ISO3, type)%>%
+  left_join(EEZ[,2:4],by= c("type", "ISO3"))%>%
+  unite("DESIGType_AREA", c(DESIG_TYPE, type)) 
 
 
+DESIG_TYPE_Out <- dcast(setDT(DESIG_TYPE.arc), ISO3 ~DESIGType_AREA, value.var = c("WDPA_AREA", "AREA_KM2"))
 
+#Read in AZE dataset subset from KBA
+AZE.arc <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/ENI-Analysis.gdb/AZE_Confirmed_DISS") %>%
+  arc.select(fields = "*")%>%
+  dplyr::select(-OBJECTID)%>%
+  dplyr::rename(ISO3 = ISO3_1) %>%
+  dplyr::rename(KBA_AREA = AREA) %>%
+  group_by(ISO3, type)%>%
+  left_join(EEZ[,2:4],by= c("type", "ISO3"))
 
-# WDPAsqlLand <- paste0("ISO3 LIKE '%",i,"%'")
-# WDPAsqlEEZ <- paste0("ISO3 LIKE '%",i,"%'")
-# 
-# WDPAsubLand <- WDPA.arc %>%
-#   arc.select(fields = "*", where_clause = WDPAsqlLand) %>%
-#   filter(STATUS %in% c("Designated", "Inscribed", "Established"))%>%
-#   arc.data2sf() %>%
-#   st_cast("POLYGON") %>%
-#   st_transform("+proj=moll") %>%
-#   st_make_valid()%>%
-#   st_intersection(Landsub)%>%
-#   st_union()
-# 
-# WDPAsubLandArea <- st_area(WDPAsubLand)/1000000
-# 
-# WDPAsubEEZ <- WDPA.arc %>%
-#   arc.select(fields = "*", where_clause = WDPAsqlEEZ) %>%
-#   filter(STATUS %in% c("Designated", "Inscribed", "Established"))%>%
-#   arc.data2sf() %>%
-#   st_cast("POLYGON") %>%
-#   st_transform("+proj=moll") %>%
-#   st_buffer(0)%>%
-#   st_intersection(Landsub)%>%
-#   st_union()
-# 
-# 
-# WDPAsubEEZArea <- st_area(WDPAsubEEZ)/1000000
+AZE_Out <-  dcast(setDT(AZE.arc), ISO3 ~type, value.var = c("KBA_AREA", "AREA_KM2"))
 
-####AZE
-AZEsql <-  paste0("ISO3 LIKE '%",i,"%'", " AND AzeStatus = 'confirmed'")
-AZEsubEEZ <- KBA.arc %>%
-  arc.select(fields = "*", where_clause = AZEsql)
-AZEsubEEZ <- AZEsubEEZ %>%
-  {if (i %in% AZEsubEEZ$ISO3) 
-  arc.data2sf() %>%
-  st_cast("POLYGON") %>%
-  st_transform("+proj=moll") %>%
-  st_buffer(0)%>%
-  st_intersection(EEZsub)%>%
-  st_union()}
+#REad in KBA dataset
+KBA.arc <- arc.open("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/ENI-Analysis.gdb/Kba_Confirmed_DISS") %>%
+  arc.select(fields = "*")%>%
+  dplyr::select(-OBJECTID)%>%
+  dplyr::rename(ISO3 = ISO3_1) %>%
+  dplyr::rename(KBA_AREA = AREA) %>%
+  group_by(ISO3, type)%>%
+  left_join(EEZ[,2:4],by= c("type", "ISO3"))
 
-if(length(AZEsubEEZ) > 0){AZEsubEEZArea <- st_area(AZEsubEEZ)/1000000}
+ KBA_Out <-  dcast(setDT(KBA.arc), ISO3 ~type, value.var = c("KBA_AREA", "AREA_KM2"))
 
-AZEsubLand <- KBA.arc %>%
-  arc.select(fields = "*", where_clause = AZEsql)
-AZEsubLand <- AZEsubLand %>%
-  {if (i %in% AZEsubLand$ISO3) 
-  arc.data2sf() %>%
-  st_cast("POLYGON") %>%
-  st_transform("+proj=moll") %>%
-  st_buffer(0)%>%
-  st_intersection(Landsub)%>%
-  st_union()}
+ #read in Critical HAbitat LAyer
+CH <- read.csv("C:/Users/OsgurM/Documents/ArcGIS/Projects/ENI-Analysis/Output/CH_Table.csv") %>%
+  dplyr::select(-c(1, 2))%>%
+  filter(CRIT_HAB != "UNCLASSIFIED")%>%
+  left_join(EEZ[,2:4], by = c("type", "ISO3") )%>%
+  unite("Type_Crit", c(type, CRIT_HAB)) 
 
-if(length(AZEsubLand) > 0){AZEsubLandArea <- st_area(AZEsubLand)/1000000}
+CH_OUT <- dcast(setDT(CH), ISO3 ~ Type_Crit, value.var = c("Count", "AREA_KM2"))
 
-####KBA 
-KBAsql <- paste0("ISO3 LIKE '%",i,"%'")
-KBAsubEEZ <- KBA.arc %>%
-  arc.select(fields = "ISO3", where_clause = KBAsql) 
-KBAsubEEZ <- KBAsubEEZ %>%
-  arc.data2sf()%>%
-  st_cast("POLYGON") %>%
-  st_transform("+proj=moll") %>%
-  st_buffer(0)%>%
-  st_intersection(EEZsub)%>%
-  st_union()
+#Output the Layers
+setwd("~/ArcGIS/Projects/ENI-Analysis/Output")
+write.csv(AZE_Out, "AZE_ENI.csv", row.names = F)
+write.csv(KBA_Out, "KBA_ENI.csv", row.names = F)
+write.csv(DESIG_Out, "WDPA_DESIG_ENI.csv", row.names = F)
+write.csv(DESIG_TYPE_Out, "WDPA_DESIG_TYPE_ENI.csv", row.names = F)
+write.csv(CH_OUT, "CritHab_ENI.csv", row.names = F)
 
-if(length(KBAsubEEZ) > 0){KBAsubEEZArea <- st_area(KBAsubEEZ)/1000000}
-
-KBAsubLand <- KBA.arc %>%
-  arc.select(fields = "ISO3", where_clause = KBAsql) 
-KBAsubLand <- KBAsubLand %>%
-  arc.data2sf()%>%
-  st_cast("POLYGON") %>%
-  st_transform("+proj=moll") %>%
-  st_buffer(0)%>%
-  st_intersection(Landsub)%>%
-  st_union()
-  
-  if(length(KBAsubLand) > 0){KBAsubLandArea <- st_area(KBAsubLand)/1000000}
-}
 
 
