7cf9ba2606609a70d0f51b939fa81fbe12cdf86c
Osgur McDermott Long <36760807+Osgur@users.noreply.github.com>
2019-09-26 16:39:08 +0100

    Update WDPA_Verified_Check.R

--- WDPA_Verified_Check.R
+++ WDPA_Verified_Check.R
@@ -27,30 +27,65 @@ ISOdata <- merge(lu_CountryNames_Jan2018[,1:2], lu_ISO2_ISO3[,1:2], by.x = "ISO3
 Path <- "C:/Users/OsgurM/Downloads/WDPA_MASTER.gdb"
 ogrListLayers(Path)
 
+ISO2 <- 
+
+CountryNames <- read.csv("C:/Users/OsgurM/OneDrive - WCMC/00_Data Management/Look_Up_Tables/lu_CountryNames_Jan2018.csv") %>%
+  select(c(1:2))
 #######Read in the data layers######
 
 #Public
+<<<<<<< HEAD
 WDPA_Py <- data.frame(read_sf(Path, layer = "WDPA_poly_Jul2019"))
 WDPA_Pt <- data.frame(read_sf(Path, layer = "WDPA_point_Jul2019"))
+=======
+WDPA_Py <- data.frame(st_read(Path, layer = "WDPA_poly_Jul2019"))
+WDPA_Pt <- data.frame(st_read(Path, layer = "WDPA_point_Jul2019"))
+>>>>>>> 5a6256f19e47156a157e4be8b2139769151e2092
 
 #Restricted for China
-China_NR_Res<- data.frame(read_sf(Path, layer = "CHN_restricted_Nov2018_NR"))
-China_NNR_Res <- data.frame(read_sf(Path, layer = "CHN_restricted_Nov2018_NNR"))
+China_NR_Res<- data.frame(st_read(Path, layer = "CHN_restricted_Nov2018_NR"))
+China_NNR_Res <- data.frame(st_read(Path, layer = "CHN_restricted_Nov2018_NNR"))
 
 #Restricted for Rusia
 Russia_Py_Res <- data.frame(read_sf(Path, layer = "RUS_only_for_public"))
 Russia_Pt_Res <- data.frame(read_sf(Path, layer = "RUSpt_only_public_New"))
 
 #Restricted for Saint Helena
-SHN_Res <- data.frame(read_sf(Path, layer = "SHN_restricted_July2018"))
+SHN_Res <- data.frame(st_read(Path, layer = "SHN_restricted_July2018"))
 
 #Restricted for Estonia
-EST_Res <- data.frame(read_sf(Path, layer = "EST_restricted_Aug2014_New"))
+EST_Res <- data.frame(st_read(Path, layer = "EST_restricted_Aug2014_New"))
 
 
 #Combine all layers
 Combined <- bind_rows(WDPA_Py, WDPA_Pt,China_NR_Res, China_NNR_Res, Russia_Py_Res, Russia_Pt_Res, SHN_Res, EST_Res)
 
+Polygons <- bind_rows(WDPA_Py, China_NNR_Res, Russia_Py_Res, SHN_Res, EST_Res) %>%  
+  select(WDPAID, ISO3, DESIG_TYPE, VERIF) %>%
+  distinct()%>%
+  group_by(ISO3) %>%
+  count(ISO3) %>% 
+  ungroup()%>%
+  mutate(ISO3 = strsplit(as.character(ISO3), ";")) %>%
+  unnest() %>%
+  group_by(ISO3) %>%
+  summarize_if(is.numeric, sum, na.rm=TRUE) %>%
+  rename(No_Polygons = n)
+  
+
+Points <- bind_rows(WDPA_Pt,China_NR_Res, Russia_Pt_Res )%>%  
+  select(WDPAID, ISO3, DESIG_TYPE, VERIF) %>%
+  distinct()%>%
+  group_by(ISO3) %>%
+  count(ISO3) %>% 
+  ungroup()%>%
+  mutate(ISO3 = strsplit(as.character(ISO3), ";")) %>%
+  unnest() %>%
+  group_by(ISO3) %>%
+  summarize_if(is.numeric, sum, na.rm=TRUE)%>%
+  rename(No_Points = n)
+
+Points_Poly <- merge(Polygons, Points, by = "ISO3" , all = T)
 
 #identify numbers for verification by ISO3
 Verified <- Combined %>%
@@ -90,6 +125,11 @@ Verified <- Verified %>%
   group_by(ISO3) %>%
   summarize_if(is.numeric, sum, na.rm=TRUE)
 
+<<<<<<< HEAD
 Verified <- merge(ISOdata, Verified, by = "ISO3", all.y = T)
+=======
+Verified <- merge(CountryNames, Verified, by = "ISO3")
+Verified <- merge(Verified, Points_Poly, by = "ISO3")
+>>>>>>> 5a6256f19e47156a157e4be8b2139769151e2092
 
 write.csv(Verified, "WDPA_Verified.csv", row.names = F)
